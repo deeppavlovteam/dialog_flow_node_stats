45af47cb3f508d734f9117c514a5e42bf31ed27f
Denis Kuznetosv <kuznetsov.den.p@gmail.com>
2021-08-05 00:37:50 +0300

    main: init

---
+++ setup.py
@@ -0,0 +1,45 @@
+import pathlib
+import setuptools
+
+
+LOCATION = pathlib.Path(__file__).parent.resolve()
+
+
+def read_requirements():
+    """parses requirements from requirements.txt"""
+    reqs_file = LOCATION / "requirements.txt"
+    reqs = [line.strip() for line in reqs_file.open(encoding="utf8").readlines() if not line.strip().startswith("#")]
+
+    names = []
+    links = []
+    for req in reqs:
+        if "://" in req:
+            links.append(req)
+        else:
+            names.append(req)
+    return {"install_requires": names, "dependency_links": links}
+
+
+readme_file = LOCATION / "README.md"
+long_description = readme_file.open(encoding="utf8").read()
+
+setuptools.setup(
+    name="dff-node-stats",
+    version="0.1.0",
+    scripts=[],
+    author="Denis Kuznetsov",
+    author_email="kuznetsov.den.p@gmail.com",
+    description="Statistics collection extension for Dialog Flow Framework (https://github.com/deepmipt/dialog_flow_framework/).",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    package_data={},
+    include_package_data=True,
+    **read_requirements()
+)
