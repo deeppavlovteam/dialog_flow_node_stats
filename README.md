c9e5824abfb04f0a5bfd1b7bf9ee6668543e1576
Denis Kuznetosv <kuznetsov.den.p@gmail.com>
2021-08-06 16:46:01 +0300

    main: add example of api

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# TODO: Description
+# Getting started
+
+Installation:
+```bash
+# basics
+pip install -e .
+# run for dashboard mode:
+pip install -r requirements_dashboard.txt
+# run for api mode:
+pip install -r requirements_api.txt
+```
+
+Examples:
+```bash
+# collect stats
+python examples/collect_stats.py
+# run dashboard:
+python examples/dashboard.py
+# run api:
+python examples/api.py
+```
--- dff_node_stats/stats.py
+++ dff_node_stats/stats.py
@@ -47,6 +47,24 @@ class Stats(BaseModel):
     def dataframe(self):
         return pd.read_csv(self.csv_file)
 
+    @property
+    def transition_counts(self):
+        df = self.dataframe.copy()
+        df["node"] = df.apply(lambda row: f"{row.flow_label}:{row.node_label}", axis=1)
+        df = df.drop(["flow_label", "node_label"], axis=1)
+        # %%
+        df = df.sort_values(["context_id"], kind="stable")
+        df["next_node"] = df.node.shift()
+        df = df[df.history_id != 0]
+        transitions = df.apply(lambda row: f"{row.node}->{row.next_node}", axis=1)
+        return {k: int(v) for k, v in dict(transitions.value_counts()).items()}
+
+    @property
+    def transition_probs(self):
+        tc = self.transition_counts
+        total = sum(tc.values(), 0)
+        return {k: v / total for k, v in tc.items()}
+
     def streamlit_run(self):
         import streamlit as st
 
@@ -56,3 +74,26 @@ class Stats(BaseModel):
         st.bar_chart(self.dataframe["node_label"].value_counts())
         st.bar_chart(self.dataframe["node_label"])
         # st.dataframe(self.dataframe)
+
+    def api_run(self, port=8000):
+        import uvicorn
+        from fastapi import FastAPI
+
+        app = FastAPI()
+
+        @app.get("/api/v1/stats/transition-counts", response_model=dict[str, int])
+        async def get_transition_counts():
+            return self.transition_counts
+
+        @app.get("/api/v1/stats/transition-probs", response_model=dict[str, float])
+        async def get_transition_probs():
+            return self.transition_probs
+
+        uvicorn.run(app, host="0.0.0.0", port=port)
+
+        # st.title("Node Analytics")
+        # st.dataframe(self.dataframe[["flow_label", "node_label"]])
+        # # st.subheader('Node labels')
+        # st.bar_chart(self.dataframe["node_label"].value_counts())
+        # st.bar_chart(self.dataframe["node_label"])
+        # # st.dataframe(self.dataframe)
